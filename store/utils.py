b38036e63df96e1adadf91e8ae158d8f9aa3a460
DESKTOP-KHMLOSQ\twipzz <dmitriygumz@gmail.com>
2021-04-13 12:57:01 +0300

    Changes: -Cart Data; -Checkout for guest(Order, Cart).

--- store/utils.py
+++ store/utils.py
@@ -14,6 +14,7 @@ def cookieCart(request):
 	cartItems = order['get_cart_items']
 
 	for i in cart:
+		
 		try:
 			cartItems += cart[i]['quantity']
 
@@ -37,3 +38,45 @@ def cookieCart(request):
 			pass
 
 	return {'cartItems':cartItems ,'order':order, 'items':items}
+
+def cartData(request):
+	if request.user.is_authenticated:
+		customer = request.user.customer
+		order, created = Order.objects.get_or_create(customer=customer, complete=False)
+		items = order.orderitem_set.all()
+		cartItems = order.get_cart_items
+	else:
+		cookieData = cookieCart(request)
+		cartItems = cookieData['cartItems']
+		order = cookieData['order']
+		items = cookieData['items']
+
+	return {'cartItems':cartItems ,'order':order, 'items':items}
+
+
+def guestOrder(request, data):
+	name = data['form']['name']
+	email = data['form']['email']
+
+	cookieData = cookieCart(request)
+	items = cookieData['items']
+
+	customer, created = Customer.objects.get_or_create(
+			email=email,
+			)
+	customer.name = name
+	customer.save()
+
+	order = Order.objects.create(
+		customer=customer,
+		complete=False,
+		)
+
+	for item in items:
+		product = Product.objects.get(id=item['id'])
+		orderItem = OrderItem.objects.create(
+			product=product,
+			order=order,
+			quantity=item['quantity'],
+		)
+	return customer, order
