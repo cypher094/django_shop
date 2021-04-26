11730df08bbc936a71c04a635b8937a7256d3e79
Oleh Haidar <oleh.haidar@gmail.com>
2021-04-26 18:58:41 +0300

    added registration/log in

--- store/urls.py
+++ store/urls.py
@@ -3,6 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+	path('register/', views.registerPage, name="register"),
+	path('login/', views.loginPage, name="login"),  
+	path('logout/', views.loginPage, name="logout"),  
+	
 	path('', views.store, name="store"),
 	path('cart/', views.cart, name="cart"),
 	path('checkout/', views.checkout, name="checkout"),
--- store/views.py
+++ store/views.py
@@ -1,10 +1,58 @@
-from django.shortcuts import render
-from django.http import JsonResponse
-import json
-import datetime
+from django.shortcuts import render, redirect
+from django.http import JsonResponse, HttpResponse
+from django.forms import inlineformset_factory
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+import json, datetime
+
 from .models import *
 from .utils import cookieCart, cartData, guestOrder
+from .forms import OrderForm, CreateUserForm
 
+from django.contrib.auth.decorators import login_required
+
+def registerPage(request):
+	form = CreateUserForm()
+
+	if request.method == 'POST':
+		form = CreateUserForm(request.POST)
+		if form.is_valid():
+			form.save()
+			user = form.cleaned_data.get('username')
+			messages.success(request, 'Account was created for ' + user)
+
+			return redirect('login')
+
+	context = {'form':form}
+	return render(request, 'store/register.html', context)
+
+
+def loginPage(request):
+	if request.user.is_authenticated:
+		return redirect('store')
+	else:
+		if request.method == 'POST':
+			username = request.POST.get('username')
+			password =request.POST.get('password')
+
+			user = authenticate(request, username=username, password=password)
+
+			if user is not None:
+				login(request, user)
+				return redirect('store')
+			else:
+				messages.info(request, 'Username OR password is incorrect')
+
+		context = {}
+		return render(request, 'store/login.html', context)
+
+@login_required(login_url='login')
+def logoutUser(request):
+	logout(request)
+	return redirect('login')
+
+@login_required(login_url='login')
 def store(request):
 	data = cartData(request)
 
@@ -16,7 +64,7 @@ def store(request):
 	context = {'products':products, 'cartItems':cartItems}
 	return render(request, 'store/store.html', context)
 
-
+@login_required(login_url='login')
 def cart(request):
 	data = cartData(request)
 
@@ -27,6 +75,7 @@ def cart(request):
 	context = {'items':items, 'order':order, 'cartItems':cartItems}
 	return render(request, 'store/cart.html', context)
 
+@login_required(login_url='login')
 def checkout(request):
 	data = cartData(request)
 
@@ -37,6 +86,7 @@ def checkout(request):
 	context = {'items':items, 'order':order, 'cartItems':cartItems}
 	return render(request, 'store/checkout.html', context)
 
+@login_required(login_url='login')
 def updateItem(request):
 	data = json.loads(request.body)
 	productId = data['productId']
@@ -62,6 +112,7 @@ def updateItem(request):
 
 	return JsonResponse('Item was added', safe=False)
 
+@login_required(login_url='login')
 def processOrder(request):
 	transaction_id = datetime.datetime.now().timestamp()
 	data = json.loads(request.body)
